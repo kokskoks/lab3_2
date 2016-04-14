3161fcb954c0aff7a170d344f4564b8cf65f90ec
Krzysztof Kadłubek <179640@edu.p.lodz.pl>
2016-04-14 17:38:56 +0200

    Finished impementation of PublishableNews

--- src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
+++ src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
@@ -12,12 +12,24 @@ import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
 
 @RunWith(PowerMockRunner.class)
-//@PrepareForTest( SingletonService.class )
+@PrepareForTest( {ConfigurationLoader.class, NewsReaderFactory.class })
 public class NewsLoaderTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		mockStatic(ConfigurationLoader.class);
+		ConfigurationLoader configLoader = mock(ConfigurationLoader.class);
+		
+		when(ConfigurationLoader.getInstance()).thenReturn(configLoader);
+		
+	}
+	
+	private IncomingInfo createIncomingInfo(SubsciptionType subscriptionType){
+		return new IncomingInfo("test test", subscriptionType);
+	}
+	
+	private IncomingNews createIncomingNews(){
+		return new IncomingNews();
 	}
 
 }
