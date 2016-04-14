38047497a558a5712e471beb59c66b6ff8f6c915
Krzysztof Kadłubek <179640@edu.p.lodz.pl>
2016-04-14 18:01:03 +0200

    added test for checking the beahaviour of loadNews method

--- src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
+++ src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
@@ -13,6 +13,7 @@ import static org.powermock.api.mockito.PowerMockito.*;
 import static org.mockito.Mockito.when;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
 import static org.hamcrest.Matchers.*;
 
 @RunWith(PowerMockRunner.class)
@@ -46,6 +47,28 @@ public class NewsLoaderTest {
 		
 		assertThat(publishableNews.getPublicContent().size(), is(4));
 		assertThat(publishableNews.getSubscribentContent().size(), is(3));
+	}
+	
+	@Test
+	public void checkIfMethodsAreCalledOnce() {
+		mockStatic(ConfigurationLoader.class);
+		ConfigurationLoader configLoader = mock(ConfigurationLoader.class);
+		when(ConfigurationLoader.getInstance()).thenReturn(configLoader);
+		when(configLoader.loadConfiguration()).thenReturn(new Configuration());
+		
+		mockStatic(NewsReaderFactory.class);
+		NewsReader newsReader = mock(NewsReader.class);
+		when(NewsReaderFactory.getReader(anyString())).thenReturn(newsReader);
+		
+		IncomingNews incomingNews = createIncomingNews();
+		
+		when(newsReader.read()).thenReturn(incomingNews);
+		
+		NewsLoader newsLoader = new NewsLoader();
+		newsLoader.loadNews();
+		
+		verify(configLoader, times(1)).loadConfiguration();
+		verify(newsReader, times(1)).read();
 	}
 	
 	
